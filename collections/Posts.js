fa75a734ae80a4db2b8b95d832ee5217f6a54584
Fernanda Tonelli <tonellibioquimica@gmail.com>
2018-06-28 23:13:42 -0300

    Implementar curtidas.

--- collections/Posts.js
+++ collections/Posts.js
@@ -2,10 +2,28 @@ Posts = new Mongo.Collection("posts");
 
 Meteor.methods({
 	"inserirPost": function(textoDoFormulario) {
-		Posts.insert({
-			texto: textoDoFormulario,
-			idDoAutor: Meteor.userId()
-		});
+		
 
+		if(Meteor.userId() !==null) {
+			Posts.insert({
+				texto: textoDoFormulario,
+				idDoAutor: Meteor.userId(),
+				curtidas: []
+			});
+		}
+	},
+	"curtirPost": function(idDoPost) {
+		Posts.update(idDoPost, {
+			$addToSet: {
+				curtidas: Meteor.userId()
+			}
+		});
+	},
+	"descurtirPost": function(idDoPost) {
+		Posts.update(idDoPost, {
+			$pull: {
+				curtidas: Meteor.userId()
+			}
+		});
 	}
 });
