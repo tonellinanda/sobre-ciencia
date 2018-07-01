e687ee245b31d64dfd2e2bcc36f44b5d68b006e9
Fernanda Tonelli <tonellibioquimica@gmail.com>
2018-07-01 16:43:21 -0300

    Implementar upload de imagens.

--- client/Feed/NovoPost.js
+++ client/Feed/NovoPost.js
@@ -1,10 +1,26 @@
+Template.NovoPost.onCreated(function() {
+	this.urlDaimagem = new ReactiveVar();
+});
+
 Template.NovoPost.events({
 	"submit form": function(evento, template) {
 		event.preventDefault();
 		var textoDoFormulario = evento.target.texto.value;
+		var urlDaimagem = template.urlDaimagem.get();
 		
-		Meteor.call("inserirPost", textoDoFormulario);
+		Meteor.call("inserirPost", textoDoFormulario, urlDaimagem);
 		
 		evento.target.texto.value = "";
-	}
+	},
+	"change .myFileInput": function(event, template) {
+        FS.Utility.eachFile(event, function(file) {
+            Images.insert(file, function (err, fileObj) {
+                if (err) {
+                    // handle error
+                } else {
+                    template.urlDaimagem.set("/cfs/files/images/" + fileObj._id);
+                }
+            });
+        });
+    }
 });
--- collections/Posts.js
+++ collections/Posts.js
@@ -1,14 +1,15 @@
 Posts = new Mongo.Collection("posts");
 
 Meteor.methods({
-	"inserirPost": function(textoDoFormulario) {
+	"inserirPost": function(textoDoFormulario, urlDaImagem) {
 		
 
 		if(Meteor.userId() !==null && textoDoFormulario) {
 			Posts.insert({
 				texto: textoDoFormulario,
 				idDoAutor: Meteor.userId(),
-				curtidas: []
+				curtidas: [],
+				imagem: urlDaImagem
 			});
 		}
 	},
