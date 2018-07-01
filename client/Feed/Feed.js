699bfd5863506e3618f9593b1f5e2ba19106acd4
Fernanda Tonelli <tonellibioquimica@gmail.com>
2018-07-01 14:47:18 -0300

    Ajustes no Cabecalho e na página inicial.

--- client/Feed/Feed.js
+++ client/Feed/Feed.js
@@ -1,7 +1,7 @@
 
 Template.Feed.helpers({
 	posts: function() {
-		var postsDaCollection = Posts.find().fetch();
+		var postsDaCollection = Posts.find().fetch().reverse();
 		return postsDaCollection;
 	}
 });
