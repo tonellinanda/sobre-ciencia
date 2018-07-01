094bd7e25505df7e3319fe673e8382de4c20f98d
Fernanda Tonelli <tonellibioquimica@gmail.com>
2018-07-01 14:15:59 -0300

    Implementar funcionalidade de seguir e deixar de seguir usuarios com ajustes.

---
+++ collections/Usuarios.js
@@ -0,0 +1,33 @@
+Meteor.methods({
+	"seguirUsuario": function(idDoUsuario) {
+		if(Meteor.userId() !== idDoUsuario) {
+			Meteor.users.update(Meteor.userId(), {
+				$addToSet: {
+					"profile.seguindo": idDoUsuario
+				}
+			});
+
+			Meteor.users.update(idDoUsuario, {
+				$addToSet: {
+					"profile.seguidores": Meteor.userId()
+				}
+			});	
+		}
+	},
+	"deixarDeSeguirUsuario": function(idDoUsuario) {
+		if(meteor.userId() !== idDoUsuario) {
+			Meteor.users.update(Meteor.userId(), {
+				$pull: {
+					"profile.seguindo": idDoUsuario
+				}
+			});
+
+			Meteor.users.update(idDoUsuario, {
+				$pull: {
+					"profile.seguidores": Meteor.userId()
+				}
+			});	
+		}	
+	}
+		
+});
